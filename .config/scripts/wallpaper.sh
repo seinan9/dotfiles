f4e442d1a46f647846c7d2200d275ee0648f29cd
Sinan Kurtyigit <sinan.kurtyigit@gmail.com>
2023-12-29 23:47:41 +0100

    Add dotfiles

---
+++ .config/scripts/wallpaper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+new_wallpaper=$1
+cache_file=$HOME/.cache/wallpaper
+wallpaper_dir=~/pictures/wallpaper
+
+if [ $# -eq 1 ]; 
+then
+    wallpaper=$new_wallpaper
+    cp $wallpaper $cache_file
+else
+    wallpaper=${wallpaper_dir}/$(ls $wallpaper_dir -1 | shuf -n 1)
+    cp $wallpaper $cache_file
+fi
+
+transition_type="left"
+
+swww img $wallpaper \
+    --transition-bezier .43,1.19,1,.4 \
+    --transition-fps=60 \
+    --transition-type=${transition_type} \
+    --transition-duration=1.5 \
+
+killall waybar
+
+wal -q -i $wallpaper
+pywalfox update
+waybar -c ~/.config/waybar/config -s ~/.config/waybar/styles.css
---
+++ .config/scripts/waybar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+killall waybar
+sleep 0.2
+waybar -c ~/.config/waybar/config -s ~/.config/waybar/styles.css
